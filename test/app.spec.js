c9e4d9a230e1418d98237632aeefe4b38cfdcc88
duq <ptd.duke@gmail.com>
2017-05-07 04:56:17 +0200

    report2 changes

--- test/app.spec.js
+++ test/app.spec.js
@@ -8,12 +8,24 @@ describe('reverseNumber', function ()
     it('sth', function(){
         expect(app.reverseNumber(2468)).to.eql(8642);
     });
+    it('sth', function(){
+        expect(app.reverseNumber("totallynotanumber")).to.eql(false);
+    });
 });
 
 describe('returnOnlyLetter', function ()
 {
     it('sth', function(){
-        expect(app.reverseNumber(2468)).to.eql(8642);
+        expect(app.returnOnlyLetter("2ez")).to.eql("ez");
+
+    });
+    it('sth', function(){
+        expect(app.returnOnlyLetter("asdf2")).to.eql("asdf");
+
+    });
+    it('sth', function(){
+        expect(app.returnOnlyLetter(123)).to.eql(false);
+
     });
 });
 
@@ -21,5 +33,8 @@ describe('isEmail', function ()
 {
     it('sth', function(){
         expect(app.isEmail("foo@gmail.com")).to.eql(true);
+    });
+    it('sth', function(){
+        expect(app.isEmail("asdfqweqew")).to.eql(false);
     });
 });
