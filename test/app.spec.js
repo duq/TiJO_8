ffbfac61ad349ed5a1d25a695c2deff58d01f2c6
duq <ptd.duke@gmail.com>
2017-05-07 04:30:35 +0200

    report1.txt changes

--- test/app.spec.js
+++ test/app.spec.js
@@ -5,15 +5,21 @@ var app = require('../app/app');
 
 describe('reverseNumber', function ()
 {
-
+    it('sth', function(){
+        expect(app.reverseNumber(2468)).to.eql(8642);
+    });
 });
 
 describe('returnOnlyLetter', function ()
 {
-
+    it('sth', function(){
+        expect(app.reverseNumber(2468)).to.eql(8642);
+    });
 });
 
 describe('isEmail', function ()
 {
-
+    it('sth', function(){
+        expect(app.isEmail("foo@gmail.com")).to.eql(true);
+    });
 });
